3977aa32a8878bd202b72270e1fd361a1d9d0636
fayazbasha974 <fayazbasha974@gmail.com>
2020-03-31 23:57:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+require('dotenv/config');
+
+app.use(bodyParser.json());
+app.use('/auth', require('./routes/auth'));
+
+app.use('/login', require('./routes/login.js'));
+app.use('/signup', require('./routes/signup.js'));
+app.use('/auth/friendRequest', require('./routes/friend-request'));
+
+mongoose.connect(process.env.mongoDB, (...args) => {
+    console.log('DB connected');
+})
+
+app.listen(process.env.port, (...args) => {
+    console.log('connected');
+});
